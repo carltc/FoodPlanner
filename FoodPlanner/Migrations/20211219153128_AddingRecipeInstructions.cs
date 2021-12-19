ea1fb55240a0940ee2c9b1b70a5c1bbffdfa181a
Carl Chittenden <carltc@hotmail.co.uk>
2021-12-19 16:04:08 +0000

    [ADD] Added instructions and source to recipes

---
+++ FoodPlanner/Migrations/20211219153128_AddingRecipeInstructions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FoodPlanner.Migrations
+{
+    public partial class AddingRecipeInstructions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstructionText",
+                table: "Recipes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RecipeSource",
+                table: "Recipes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstructionText",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "RecipeSource",
+                table: "Recipes");
+        }
+    }
+}
